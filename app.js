a8f9d82a74493f0c60dc181a0e02043443fb87e0
Dillon <dmanhall@gmail.com>
2019-09-05 18:51:41 -0600

    added functions for reading a cookie down from the page.

--- app.js
+++ app.js
@@ -20,12 +20,41 @@ function initMap() {
 (function(w){
 
 
-    //debugger;
+    // helper function for cookies from W3 schools
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
     
-
-
+    // cookies are needed for reading the API key from the customer
+    // this function checks for the needed cookie and returns true if that cookie exists
+    function hasKey(){
+        var validKey = false;
+        var key = getCookie("apikey");
+        if(key != ""){
+            validkey = true;
+        }
+        return validkey;
+    }
+    
     $(document).ready(function(){
       console.log("page ready");
+      console.log("checking for valid api key");
+      if(hasKey(){ // check for the api key
+         var apicode = getCookie("apikey");
+         // TODO: add the API key to the div tag in the index page
+      }
     });
 
 })(Window);
